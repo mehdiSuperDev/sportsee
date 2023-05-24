fc1ac4727eb37df32837b69029ecf9b80c1d892a
Mehdi Raheemun <mraheemu@student.42.fr>
2023-05-24 12:27:42 +0200

    wip Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import styles from './Card.module.css';
+import hamburger from '../../assets/images/hamburger.png';
+import chicken from '../../assets/images/chicken.png';
+import apple from '../../assets/images/apple.png';
+import fire from '../../assets/images/fire.png';
+
+const getCardComponent = (value, label, iconName) => {
+  return (
+    <div className={styles.container}>
+      <img src={iconName} />
+      <div className={styles.information}>
+        <p>{value}</p>
+        <p>{label}</p>
+      </div>
+    </div>
+  );
+};
+
+function Card({ nutrientTypeEnum, value }) {
+  switch (nutrientTypeEnum) {
+    case 'glucide':
+      return getCardComponent(value, 'Glucide', apple);
+    case 'proteine':
+      return getCardComponent(value, 'Proteine', chicken);
+    case 'lipides':
+      return getCardComponent(value, 'Lipides', hamburger);
+    case 'calories':
+      return getCardComponent(value, 'Calories', fire);
+    default:
+      break;
+  }
+}
+
+Card.propTypes = {
+  nutrientTypeEnum: PropTypes.oneOf([
+    'glucide',
+    'calories',
+    'lipides',
+    'proteines',
+  ]).isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import Header from '../components/Header/Header';
 import BarChartActivity from '../components/BarChart/BarChartActivity';
-import ActivityScore from '../components/ActivityScore/ActivityScore';
+// import ActivityScore from '../components/ActivityScore/ActivityScore';
+import Card from '../components/Card/Card';
 
 function Home() {
   return (
     <>
       <Header />
       <BarChartActivity />
-      <ActivityScore score={12} />
+      {/* <ActivityScore score={12} /> */}
+      <p>SPACER</p>
+      <Card nutrientTypeEnum="glucide" value={12} />
     </>
   );
 }
