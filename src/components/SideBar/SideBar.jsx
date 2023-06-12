f0bf9ae28e3ec9ce3304a81e64f9cd6a36233880
Mehdi Raheemun <mraheemu@student.42.fr>
2023-06-12 15:37:36 +0200

    update SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,22 @@
 import styles from './SideBar.module.css';
+import yoga from '../../../src/assets/images/yoga.png';
+import swim from '../../../src/assets/images/swim.png';
+import cycle from '../../../src/assets/images/cycle.png';
+import alter from '../../../src/assets/images/alter.png';
 
 export default function SideBar() {
+  const images = [yoga, swim, cycle, alter];
+
   return (
     <div className={styles.container}>
-      <p>Hello world</p>
+      {images.map((image, index) => {
+        return (
+          <div className={styles.item} key={index}>
+            <img src={image} alt={`icon ${index}`} />
+          </div>
+        );
+      })}
+      <p>Copiryght, SportSee 2020</p>
     </div>
   );
 }
